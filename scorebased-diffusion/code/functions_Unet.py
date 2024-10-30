5108f78ec9974ee657f8af38061cf92f11c9ec8f
nguyenthuynga <nttnga@math.ac.vn>
2024-10-30 16:09:07 +0100

    add Unet

---
+++ scorebased-diffusion/code/functions_Unet.py
@@ -0,0 +1,395 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+
+def one_param(m):
+    "get model first parameter"
+    return next(iter(m.parameters()))
+
+
+class GaussianFourierProjection(nn.Module):
+    def __init__(self, embed_dim, scale=30.):
+        super().__init__()
+    # Randomly sample weights during initialization. These weights are fixed
+    # during optimization and are not trainable.
+        self.W = nn.Parameter(torch.randn(embed_dim // 2) * scale, requires_grad=False)
+    def forward(self, x):
+        x_proj = x[:, None] * self.W[None, :] * 2 * math.pi
+        return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
+
+
+class Dense(nn.Module):
+    """A fully connected layer that reshapes outputs to feature maps.
+  Allow time repr to input additively from the side of a convolution layer.
+  """
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.dense = nn.Linear(input_dim, output_dim)
+    def forward(self, x):
+        return self.dense(x)[..., None, None]
+
+class SelfAttention(nn.Module):
+    def __init__(self, channels):
+        super(SelfAttention, self).__init__()
+        self.channels = channels        
+        self.mha = nn.MultiheadAttention(channels, 4, batch_first=True)
+        self.ln = nn.LayerNorm([channels])
+        self.ff_self = nn.Sequential(
+            nn.LayerNorm([channels]),
+            nn.Linear(channels, channels),
+            nn.GELU(),
+            nn.Linear(channels, channels),
+        )
+
+    def forward(self, x):
+        #print('x', x.shape)
+        size = x.shape[-1]
+        size1 = x.shape[2]
+        #print('size', size1)
+        #print(self.channels)
+        x = x.view(-1, self.channels, size1 * size).swapaxes(1, 2)
+        x_ln = self.ln(x)
+        attention_value, _ = self.mha(x_ln, x_ln, x_ln)
+        attention_value = attention_value + x
+        attention_value = self.ff_self(attention_value) + attention_value
+        return attention_value.swapaxes(2, 1).view(-1, self.channels, size1, size)
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None, residual=False):
+        super().__init__()
+        self.residual = residual
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.GroupNorm(1, mid_channels),
+            nn.GELU(),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.GroupNorm(1, out_channels),
+        )
+
+    def forward(self, x):
+        if self.residual:
+            return F.gelu(x + self.double_conv(x))
+        else:
+            return self.double_conv(x)
+
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels, emb_dim=256):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, in_channels, residual=True),
+            DoubleConv(in_channels, out_channels),
+        )
+
+        self.emb_layer = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(
+                emb_dim,
+                out_channels
+            ),
+        )
+
+    def forward(self, x, t):
+        x = self.maxpool_conv(x)
+        emb = self.emb_layer(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        return x + emb
+
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, emb_dim=256):
+        super().__init__()
+
+        self.up = nn.Upsample(size=[25, 90], mode="bilinear", align_corners=True)
+        self.conv = nn.Sequential(
+            DoubleConv(in_channels, in_channels, residual=True),
+            DoubleConv(in_channels, out_channels, in_channels // 2),
+        )
+
+        self.emb_layer = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(
+                emb_dim,
+                out_channels
+            ),
+        )
+
+    def forward(self, x, skip_x, t):
+        x = self.up(x)
+        x = torch.cat([skip_x, x], dim=1)
+        x = self.conv(x)
+        emb = self.emb_layer(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        return x + emb
+class Up2(nn.Module):
+    def __init__(self, in_channels, out_channels, emb_dim=256):
+        super().__init__()
+
+        self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        self.conv = nn.Sequential(
+            DoubleConv(in_channels, in_channels, residual=True),
+            DoubleConv(in_channels, out_channels, in_channels // 2),
+        )
+
+        self.emb_layer = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(
+                emb_dim,
+                out_channels
+            ),
+        )
+
+    def forward(self, x, skip_x, t):
+        x = self.up(x)
+        x = torch.cat([skip_x, x], dim=1)
+        x = self.conv(x)
+        emb = self.emb_layer(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        return x + emb
+
+class UNet(nn.Module):
+    def __init__(self, c_in=1, c_out=1, time_dim=256, remove_deep_conv=False):
+        super().__init__()
+        self.time_dim = time_dim
+        self.remove_deep_conv = remove_deep_conv
+        self.inc = DoubleConv(c_in, 64)
+        self.down1 = Down(64, 128)
+        self.sa1 = SelfAttention(128)
+        self.down2 = Down(128, 256)
+        self.sa2 = SelfAttention(256)
+        self.down3 = Down(256, 256)
+        self.sa3 = SelfAttention(256)
+
+
+        if remove_deep_conv:
+            self.bot1 = DoubleConv(256, 256)
+            self.bot3 = DoubleConv(256, 256)
+        else:
+            self.bot1 = DoubleConv(256, 512)
+            self.bot2 = DoubleConv(512, 512)
+            self.bot3 = DoubleConv(512, 256)
+
+        self.up1 = Up(512, 128)
+        self.sa4 = SelfAttention(128)
+        self.up2 = Up2(256, 64)
+        self.sa5 = SelfAttention(64)
+        self.up3 = Up2(128, 64)
+        self.sa6 = SelfAttention(64)
+        self.outc = nn.Conv2d(64, c_out, kernel_size=1)
+
+    def pos_encoding(self, t, channels):
+        inv_freq = 1.0 / (
+            10000
+            ** (torch.arange(0, channels, 2, device=one_param(self).device).float() / channels)
+        )
+        pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc
+
+    def unet_forwad(self, x, t):
+        x1 = self.inc(x)
+        x2 = self.down1(x1, t)
+        #print('x2', x2.shape)
+        x2 = self.sa1(x2)
+        x3 = self.down2(x2, t)
+        x3 = self.sa2(x3)
+        #print('x3', x3.shape)
+        x4 = self.down3(x3, t)
+        x4 = self.sa3(x4)
+
+        x4 = self.bot1(x4)
+        #print('x4_before', x4.shape)
+        if not self.remove_deep_conv:
+            x4 = self.bot2(x4)
+        x4 = self.bot3(x4)
+        #print('x4', x4.shape)
+        x = self.up1(x4, x3, t)
+        x = self.sa4(x)
+        x = self.up2(x, x2, t)
+        x = self.sa5(x)
+        x = self.up3(x, x1, t)
+        ##x = self.sa6(x)                #####Removed ###Add after training # add after normalization
+        output = self.outc(x)
+        return output
+    
+    def forward(self, x, t):
+        t = t.unsqueeze(-1)
+        t = self.pos_encoding(t, self.time_dim)
+        return self.unet_forwad(x, t)
+
+class CrossAttention(nn.Module):
+    def __init__(self, embed_dim, hidden_dim, context_dim=None, num_heads=1):
+        super(CrossAttention, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.context_dim = context_dim
+        self.embed_dim = embed_dim
+        self.query = nn.Linear(hidden_dim, embed_dim, bias=False)
+        if context_dim is None:
+            # Self Attention
+            self.key = nn.Linear(hidden_dim, embed_dim, bias=False)
+            self.value = nn.Linear(hidden_dim, hidden_dim, bias=False)
+            self.self_attn = True
+        else:
+            # Cross Attention
+            self.key = nn.Linear(context_dim, embed_dim, bias=False)
+            self.value = nn.Linear(context_dim, hidden_dim, bias=False)
+            self.self_attn = False
+        # self.query = nn.Conv1d(hidden_dim, embed_dim, 1, bias=False)
+        # if context_dim is None:
+        #   self.key = nn.Conv1d(hidden_dim, embed_dim, 1, bias=False)
+        #   self.value = nn.Conv1d(hidden_dim, hidden_dim, 1, bias=False)
+        #   self.self_attn = True
+        # else:
+        #   self.key = nn.Conv1d(context_dim, embed_dim, 1, bias=False)
+        #   self.value = nn.Conv1d(context_dim, hidden_dim, 1, bias=False)
+        #   self.self_attn = False
+
+    def forward(self, tokens, context=None):
+        Q = self.query(tokens)
+        K = self.key(tokens) if self.self_attn else self.key(context)
+        V = self.value(tokens) if self.self_attn else self.value(context)
+        # if self.self_attn:
+        # print(Q.shape, K.shape, V.shape)
+        scoremats = torch.einsum("BTH,BSH->BTS", Q, K)
+        attnmats = F.softmax(scoremats / math.sqrt(self.embed_dim), dim=-1)
+        # print(scoremats.shape, attnmats.shape, )
+        ctx_vecs = torch.einsum("BTS,BSH->BTH", attnmats, V)
+        return ctx_vecs
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, hidden_dim, context_dim):
+        super(TransformerBlock, self).__init__()
+        self.attn_self = CrossAttention(hidden_dim, hidden_dim, )
+        self.attn_cross = CrossAttention(hidden_dim, hidden_dim, context_dim)
+
+        self.norm1 = nn.LayerNorm(hidden_dim)
+        self.norm2 = nn.LayerNorm(hidden_dim)
+        self.norm3 = nn.LayerNorm(hidden_dim)
+        self.ffn = nn.Sequential(
+            nn.Linear(hidden_dim, 3 * hidden_dim),
+            nn.GELU(),
+            nn.Linear(3 * hidden_dim, hidden_dim)
+        )
+
+
+    def forward(self, x, context=None):
+        x = self.attn_self(self.norm1(x)) + x
+        x = self.attn_cross(self.norm2(x), context=context) + x
+        x = self.ffn(self.norm3(x)) + x
+        return x
+
+
+class SpatialTransformer(nn.Module):
+    def __init__(self, hidden_dim, context_dim):
+        super(SpatialTransformer, self).__init__()
+        self.transformer = TransformerBlock(hidden_dim, context_dim)
+
+    def forward(self, x, context=None):
+        b, c, h, w = x.shape
+        x_in = x
+        # context = rearrange(context, "b c T -> b T c")
+        x = rearrange(x, "b c h w->b (h w) c")
+        x = self.transformer(x, context)
+        x = rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
+        return x + x_in
+
+
+class UNet_Tranformer_attrb1(nn.Module):
+    """A time-dependent score-based model built upon U-Net architecture."""
+
+    def __init__(self, marginal_prob_std, channels=[32, 64, 128, 256], embed_dim=256,
+                 text_dim=1, nAttr=40):
+        """Initialize a time-dependent score-based network.
+
+        Args:
+          marginal_prob_std: A function that takes time t and gives the standard
+            deviation of the perturbation kernel p_{0t}(x(t) | x(0)).
+          channels: The number of channels for feature maps of each resolution.
+          embed_dim: The dimensionality of Gaussian random feature embeddings.
+        """
+        super().__init__()
+        # Gaussian random feature embedding layer for time
+        self.embed = nn.Sequential(GaussianFourierProjection(embed_dim=embed_dim),
+                                   nn.Linear(embed_dim, embed_dim))
+        # Encoding layers where the resolution decreases
+        self.conv1 = nn.Conv2d(2, channels[0], 3, stride=1, bias=False)#change 25a: making 2 instead of 9 as Mahima code
+        self.dense1 = Dense(embed_dim, channels[0])
+        self.gnorm1 = nn.GroupNorm(4, num_channels=channels[0])
+        self.conv2 = nn.Conv2d(channels[0], channels[1], 3, stride=2, bias=False, )
+        self.dense2 = Dense(embed_dim, channels[1])
+        self.gnorm2 = nn.GroupNorm(32, num_channels=channels[1])
+        self.conv3 = nn.Conv2d(channels[1], channels[2], 3, stride=2, bias=False, )
+        self.dense3 = Dense(embed_dim, channels[2])
+        self.gnorm3 = nn.GroupNorm(32, num_channels=channels[2])
+        self.attn3 = SpatialTransformer(channels[2], text_dim)
+        self.conv4 = nn.Conv2d(channels[2], channels[3], 3, stride=2, bias=False, )
+        self.dense4 = Dense(embed_dim, channels[3])
+        self.gnorm4 = nn.GroupNorm(32, num_channels=channels[3])
+        self.attn4 = SpatialTransformer(channels[3], text_dim)
+
+        # Decoding layers where the resolution increases
+        self.tconv4 = nn.ConvTranspose2d(channels[3], channels[2], 3, stride=2, bias=False, output_padding=(1,1)) #change 25b: add padding here
+        self.dense5 = Dense(embed_dim, channels[2])
+        self.tgnorm4 = nn.GroupNorm(32, num_channels=channels[2])
+        self.attn5 = SpatialTransformer(channels[2], text_dim)
+        self.tconv3 = nn.ConvTranspose2d(channels[2], channels[1], 3, stride=2, bias=False,
+                                         output_padding=1)  # , output_padding=1)     #  + channels[2]
+        self.dense6 = Dense(embed_dim, channels[1])
+        self.tgnorm3 = nn.GroupNorm(32, num_channels=channels[1])
+        self.tconv2 = nn.ConvTranspose2d(channels[1], channels[0], 3, stride=2, bias=False,
+                                         output_padding=1)  # , output_padding=1)     #  + channels[1]
+        self.dense7 = Dense(embed_dim, channels[0])
+        self.tgnorm2 = nn.GroupNorm(32, num_channels=channels[0])
+        self.tconv1 = nn.ConvTranspose2d(channels[0], 1, 3, stride=1)  # + channels[0]
+
+        # The swish activation function
+        self.act = nn.SiLU()  # lambda x: x * torch.sigmoid(x)
+        self.marginal_prob_std = marginal_prob_std
+        self.cond_embed = nn.Embedding(nAttr + 1, text_dim, padding_idx=nAttr)  # +1 for the padding index
+
+    def forward(self, x, t, y=None):
+        # Obtain the Gaussian random feature embedding for t
+        embed = self.act(self.embed(t))
+        # print('y', y.shape)
+        #y_embed = self.cond_embed(y)#.unsqueeze(4)
+        #print('y_embed', y_embed.shape)
+        # Encoding path
+        x= torch.cat((x, y), 1)
+        h1 = self.conv1(x) + self.dense1(embed)
+        ## Incorporate information from t
+        ## Group normalization
+        h1 = self.act(self.gnorm1(h1))
+        h2 = self.conv2(h1) + self.dense2(embed)
+        h2 = self.act(self.gnorm2(h2))
+        h3 = self.conv3(h2) + self.dense3(embed)
+        #print('h3', h3.shape)
+        h3 = self.act(self.gnorm3(h3))
+        # h3 = self.attn3(h3, y_embed)
+        #h3 = self.attn3(h3, embed)
+        h4 = self.conv4(h3) + self.dense4(embed)
+        h4 = self.act(self.gnorm4(h4))
+        #h4 = self.attn4(h4, y_embed)
+        #print('h4', h4.shape)
+        
+        
+        # Decoding path
+        h = self.tconv4(h4) + self.dense5(embed)
+        #print('h', h.shape)
+        ## Skip connection from the encoding path
+        h = self.act(self.tgnorm4(h))
+        # h = self.attn5(h, y_embed)
+        #h = self.attn5(h, embed)
+        h = self.tconv3(h + h3) + self.dense6(embed)
+        h = self.act(self.tgnorm3(h))
+        h = self.tconv2(h + h2) + self.dense7(embed)
+        h = self.act(self.tgnorm2(h))
+        h = self.tconv1(h + h1)
+
+        # Normalize output
+        h = h / self.marginal_prob_std(t)[:, None, None, None]
+        return h
